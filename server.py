51d08cb26af189609e9f7e8fb0d9ee945524c2b2
MathlaAlkuwari <mathlaalkuwari.23@gmail.com>
2025-12-16 19:45:32 +0300

    Update server.py

--- server.py
+++ server.py
@@ -37,3 +37,35 @@ while True:
 try: 
   msg = conn.recv(1024).decode("utf-8").strip()
   print("client says:", msg) 
+  
+if not msg: 
+  conn.sendall("empty request.".encode("utf-8))
+
+ elif msg == "1":
+ # client wants a cat fact 
+ fact= get_cat_fact()
+
+ # save to json file
+ date = {"fact": fact}
+ json_path = os.path.join(RESULTS_DIR, "cat_fact_json")
+ with open(json_path, "w", encoding="utf-8") as f :
+ json.dump (data ,f, indent=4, ensure_ascii=False)
+
+conn.sendall(fact.encode("utf-8"))
+
+elif msg == "2":
+# client wants to exit
+conn.sendall("goodbye!".encode("utf-8"))
+
+else:
+conn.sendall("invalid option".encode("utf-8"))
+
+except Exception as e:
+# trying to send something back if possible 
+try: 
+conn.sendall(f"server error: {e}".encode("utf-8"))
+except exception :
+pass
+
+finally:
+conn.close()
